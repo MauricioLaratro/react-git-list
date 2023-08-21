387e5c92dc679599202336ebc341bec2cb0b4d87
unknown <Mauricio Laratro>
2023-08-21 17:13:46 -0300

    Creacion del componente layout y sus childrens, estilos con css-grid-layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components'
 
 const LayoutStyled = styled.main`
-
+    min-block-size: 100vh;
+    margin: auto;
+    max-inline-size: 75rem;
+    padding-inline: 20px;
+    @media screen and (min-width: 768px) {
+        display: grid;
+        border: 1px solid red;
+        grid-template-columns: 278px 1fr;
+        grid-template-rows: auto 1fr;
+        column-gap: 2rem;
+        grid-template-areas: "profile filters" "profile repo-list";
+    }
 `
 
 function Layout({ children }) {
--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const SearchStyled = styled.div`
-
+    background-color: purple;
 `
 
 function Search() {
