019ca211061d260eace21fa42b1cb09b7a05581e
unknown <Mauricio Laratro>
2023-08-28 20:57:27 -0300

    Creamos dimos estilos y consumimos la informacion necesaria, para el componenete de repo-list

--- src/components/repo-list.js
+++ src/components/repo-list.js
@@ -1,14 +1,21 @@
 import styled from 'styled-components'
+import RepoItem from './repo-item'
+
 
 const RepoListStyled = styled.div`
     grid-area: repo-list;
-    background-color: pink;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
 `
 
-function RepoList() {
+function RepoList({ repoList }) {
     return (
         <RepoListStyled>
-            RepoList
+            {repoList.map((item) =>{
+                return <RepoItem {...item} key={item.id} />
+            })}
         </RepoListStyled>
     )
 }
